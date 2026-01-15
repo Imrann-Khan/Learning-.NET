bfa3afc86d3ca5a3b52e1584e7b146bb4ec3a6a0
Imrann-Khan <ik11150500218@gmail.com>
2026-01-16 03:43:02 +0600

    async/await practice

--- Asynchronus Programming/Program.cs
+++ Asynchronus Programming/Program.cs
@@ -1,2 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+using System;
 
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Main method started..");
+        //Method1();
+        Method2();
+        Console.WriteLine("Main method ended.");
+    }
+
+    public static void Method1() //Without asynchronus programming
+    {
+        Console.WriteLine("Method1 started....");
+        Thread.Sleep(5000);
+        Console.WriteLine("Methid1 ended.");
+    }
+
+    public async static void Method2() //Asynchronus programming
+    {
+        Console.WriteLine("Method1 started....");
+        //await Thread.Sleep(10000);
+        await Task.Delay(5000);
+        Console.WriteLine("Methid1 ended.");
+    }
+}
