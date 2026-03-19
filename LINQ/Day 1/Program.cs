aed1f8f306dcb8e4d56ebe9236ca6c7fdab239d5
Imrann-Khan <ik11150500218@gmail.com>
2026-03-20 04:25:39 +0600

    LINQ practice day 1

---
+++ LINQ/Day 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // Data Source
+        List<int> list = new List<int>(){
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+        };
+
+        // // 1. Query (Query Syntax)
+        // var QuerySyntax = from i in list
+        //                     where i >= 5
+        //                     select i;
+        
+        // 2. Query (Method Syntax)
+        var QuerySyntax = list.Where(i => i>=5).ToList();
+
+        // Execution
+        foreach(var i in QuerySyntax)
+        {
+            Console.Write($"{i} ");
+        }
+    }
+}
